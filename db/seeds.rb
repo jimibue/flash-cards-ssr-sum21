d1ad9b22458ac4c4e6f5dabdceead8362283f0b6
James Yeates <jimibue@gmail.com>
2021-05-27 09:59:47 -0600

    db setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Category.destroy_all
+
+js = Category.create(name:'JavaScript')
+science = Category.create(name:"Science")
+random = Category.create(name:'Random')
+
+js.cards.create(question:'True or false, let is preffered over var', answer:'false', answered:false)
+js.cards.create(question:"1 == '1' ", answer:'true', answered:false)
+
+science.cards.create(question:"What is the max speed limit in the universe ", answer:'speed of light', answered:false)
+science.cards.create(question:"What is the symbo for water", answer:'H20', answered:false)
+
+
+
