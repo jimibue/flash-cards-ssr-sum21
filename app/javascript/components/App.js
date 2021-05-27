3a5e9a532becd7d488376580414537e00f37a1cb
James Yeates <jimibue@gmail.com>
2021-05-27 12:40:06 -0600

    crud on categories

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,5 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
+import Category from './Category'
+import CategoryForm from './CategoryForm'
 
 const App = ()=>{
     const [categories, setCategories] = useState([])
@@ -9,10 +11,38 @@ const App = ()=>{
         getCategories()
     }, [])
 
+    // method update UI
+    const addCategory = (category) => {
+        let updatedCategories = [category, ...categories]
+        setCategories(updatedCategories)
+    }
+
+      // method update UI
+    const updateCategory = (category) => {
+        let updatedCategories = categories.map( c=>{
+            if(c.id === category.id){
+                return category
+            } 
+            return c
+        } )
+
+        setCategories(updatedCategories)
+    }
+
     const getCategories = async () => {
         let res = await axios.get('/categories')
         // let {data} = await axios.get('/categories')
         setCategories(res.data)
+    }
+
+    const deleteCategory = async (id) => {
+        console.log(id)
+        // delete from db
+        let res = await axios.delete(`/categories/${id}`)
+        // remove from UI
+        let updatedCategories = categories.filter( c => c.id !== res.data.id )
+        setCategories(updatedCategories)
+
     }
 
     const renderCategories = ()=>{
@@ -20,17 +50,14 @@ const App = ()=>{
             return <p>no categories</p>
         }
         return categories.map(c => {
-            return (
-                <div key={c.id}>
-                    <h1>{c.name}</h1>
-                </div>
-            )
+            return <Category deleteCategory={deleteCategory} key={c.id} {...c} updateCategory={updateCategory}/>
         })
     }
     return (
         <div>
             <h1>App</h1>
             <p>front end guy you go from here</p>
+            <CategoryForm addCategory={addCategory} />
             {renderCategories()}
         </div>
     )
---
+++ app/javascript/components/Category.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react'
+import CategoryForm from "./CategoryForm"
+
+const Category = (props) => {
+    const {id, name, updateCategory, deleteCategory} = props
+    const [showForm, setShowForm] = useState(false)
+    
+    return (
+        <div>
+            <h1>{name}</h1>
+             <p onClick={() => deleteCategory(id)} style={{color:'red'}}>delete</p>
+             <p onClick={() => setShowForm(!showForm)}>toggle</p>
+             {showForm && <CategoryForm id={id} name={name} updateCategory={updateCategory}/>}
+        </div>
+    )
+}
+
+export default Category
