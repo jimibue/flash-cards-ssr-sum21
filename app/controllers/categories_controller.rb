8d834174c05c345766bf17db03eb475c74ba0071
James Yeates <jimibue@gmail.com>
2021-05-27 10:37:19 -0600

    show methods done

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,21 @@
 class CategoriesController < ApplicationController
+    before_action :set_category, only:[:show]
+    ## ssr of a react component
+    def app
+     render component: 'App'
+    end
+
+    def index
+      render json: Category.all
+    end
+
+    def show
+       render json: {category: @category, cards:@category.cards}
+    end
+
+    private
+
+    def set_category
+        @category = Category.find(params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-  resources :categories
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'categories#app'
+  # resources :categories
+
+  get '/categories', to:'categories#index'
+  get '/categories/:id', to:'categories#show'
+
+  get '/categories/:category_id/cards', to: 'cards#index'
+  get '/categories/:category_id/cards/:id', to: 'cards#show'
+
+  get  '/cards', to:'cards#all'
 end
